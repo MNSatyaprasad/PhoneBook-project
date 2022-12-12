0ff8aac0039744c911cd2f1a22dc7397a077d776
unknown <sales.baireddytc@gmail.com>
2022-12-12 10:56:11 +0530

    Rest controller and H2 database is added

--- src/main/java/com/phonebook/controller/ContactController.java
+++ src/main/java/com/phonebook/controller/ContactController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,25 +21,31 @@ public class ContactController {
 	@Autowired
 	private IContactService contactService;
 	
-	@PostMapping("/save")
-	public String saveContact(@RequestBody Contact contact) {
+	@PostMapping("/contact")
+	public String createContact(@RequestBody Contact contact) {
 		String savecontact =contactService.saveContact(contact);
 		return savecontact;
 	}
 	
-	@GetMapping("/getAllContacts")
+	@GetMapping("/contacts")
 	public List<Contact> getAllContacts(){
 		List<Contact> allContacts =contactService.getAllContacts();
 		return allContacts;
 	}
-	@GetMapping("/{id}")
-	public Optional<Contact> getContactById(@PathVariable("id") Integer contactId){
-		Optional<Contact> contactById = contactService.getContactById(contactId);
+	@GetMapping("/contact/{contactId}")
+	public Contact getContactById(@PathVariable Integer contactId){
+		Contact contactById = contactService.getContactById(contactId);
 		return contactById;
 	}
+	@PutMapping("/contact")
 	public String updateContact(@RequestBody Contact contact) {
 		String updateContact =contactService.updateContact(contact);
 		return updateContact;
 	}
 
+	@DeleteMapping("/contact/{contactId}")
+	public String deleteContact(@PathVariable Integer contactId) {
+		String status = contactService.deletecontactById(contactId);
+		return status;
+	}
 }
